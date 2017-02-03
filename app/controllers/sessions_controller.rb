13eb79f36c9a7264624410186b7414670ecaa897
Eben <eben.lowe@gmail.com>
2017-02-03 09:38:40 -0500

    Move extracted session#create logic from controller to User model.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
 
   def create
-    user_hash = request.env["omniauth.auth"].slice("uid", "info", "extra")
-    existing_user = User.find_by(github_uid: user_hash["uid"])
+    auth_hash = request.env["omniauth.auth"].slice("uid", "info", "extra")
+    existing_user = User.find_by(github_uid: auth_hash["uid"])
     if existing_user.nil?
-      user = new_user_from_github(user_hash)
+      user = User.new_from_github(auth_hash)
     else
-      user = updated_user_from_github(existing_user, user_hash)
+      user = existing_user.update_from_github(auth_hash)
     end
     if user.save
       sign_in(user)
@@ -16,26 +16,6 @@ class SessionsController < ApplicationController
       flash[:alert] = "There was a problem signing in."
     end
     redirect_to root_path
-  end
-
-  protected
-
-  def new_user_from_github(user_hash)
-    User.new(
-        email: user_hash["info"]["email"],
-        github_uid: user_hash["uid"],
-        image_url: user_hash["extra"]["raw_info"]["avatar_url"],
-        name: user_hash["info"]["name"],
-        sign_in_count: 1)
-  end
-
-  def updated_user_from_github(user, user_hash)
-    user.assign_attributes(
-      email: user_hash["info"]["email"],
-      image_url: user_hash["extra"]["raw_info"]["avatar_url"],
-      name: user_hash["info"]["name"])
-    user.increment :sign_in_count
-    user
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+  EMAIL_REGEXP = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  validates_format_of :email, with: EMAIL_REGEXP, allow_blank: true
+  validates_presence_of :image_url, :name
+  validates_uniqueness_of :github_uid
+
+  def update_from_github(auth_hash)
+    assign_attributes(
+      image_url: auth_hash["extra"]["raw_info"]["avatar_url"],
+      name: auth_hash["info"]["name"])
+    increment :sign_in_count
+    self
+  end
+
+  def self.new_from_github(auth_hash)
+    new(
+      github_uid: auth_hash["uid"],
+      image_url: auth_hash["extra"]["raw_info"]["avatar_url"],
+      name: auth_hash["info"]["name"],
+      sign_in_count: 1)
+  end
+
 end
