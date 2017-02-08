df992da9bd635e5b94d2d8fe85525eb39fe061fa
Eben <eben.lowe@gmail.com>
2017-02-08 16:25:59 -0500

    First stab at setting up react-router-redux (no reducers.)

---
+++ react/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component }  from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <h1>Hello from the <code>App.js</code> component.</h1>
+    )
+  }
+}
+
+export default App;
--- react/src/main.js
+++ react/src/main.js
@@ -1,10 +1,32 @@
 import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import App from './components/App'
+
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  })
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 $(function() {
-  ReactDOM.render(
-    <h1>Hello from <code>main.js</code></h1>,
-    document.getElementById('app')
-  );
+  let reactElement = document.getElementById('app');
+  if (reactElement) {
+    ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </Provider>,
+    reactElement
+    );
+  };
 });
