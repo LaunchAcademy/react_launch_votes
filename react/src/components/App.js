fe0fa79ea94c09dcc5cdc44c19b60d6f5308f5ae
Eben <eben.lowe@gmail.com>
2017-02-16 11:35:55 -0500

    Remove 'hello from App component' message.

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -7,12 +7,7 @@ class App extends Component {
 
   render() {
     return(
-      <div className="row">
-        <div className="small-12 columns">
-          <h1>Hello from the <code>App.js</code> component.</h1>
-          {this.props.children}
-        </div>
-      </div>
+      <div>{this.props.children}</div>
     )
   }
 }
