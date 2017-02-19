a3d894486fb1172a62ec82be09b6c465316b14cb
Eben <eben.lowe@gmail.com>
2017-02-18 23:29:16 -0500

    Scope nominations to current week only.

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,8 +1,12 @@
 class Nomination < ApplicationRecord
 
-  default_scope { by_nominee.newest_first }
+  default_scope { current_week.by_nominee.newest_first }
   scope :by_nominee, -> { joins(:nominee).order('name') }
   scope :newest_first, -> { order(created_at: :desc) }
+  scope :current_week,
+    -> { where("nominations.created_at > ?", Time.current.beginning_of_week) }
+  scope :previous_weeks,
+    -> { where("nominations.created_at < ?", Time.current.beginning_of_week) }
 
   belongs_to :nominator, class_name: "User"
   belongs_to :nominee, class_name: "User"
