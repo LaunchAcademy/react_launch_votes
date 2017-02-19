eb1bb290bd779cf3dbc2a5648f73af756f8e66da
Eben <eben.lowe@gmail.com>
2017-02-19 11:35:14 -0500

    Create user profiles, abstract awards to partial.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find_by(handle: params[:id])
+    @awards = Nomination.unscoped.where(nominee: @user).by_votes(1)
+  end
 end
