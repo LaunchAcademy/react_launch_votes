0d4c7f2a6df80a6ae7cc928d3c28ee4d409b4a2f
Eben <eben.lowe@gmail.com>
2017-03-10 23:59:54 -0500

    Adjust team selector logic so that single-team admins with no default team get to team selector.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,12 @@ class PagesController < ApplicationController
   def index
     if user_signed_in?
       unless current_user.launch_pass_id.nil?
-        if current_user.teams.count == 1
-          redirect_to team_nominations_path(current_user.teams.first)
+        if current_user.admin? && current_user.default_team.nil?
+          redirect_to team_selector_path
         elsif !current_user.default_team.nil?
           redirect_to team_nominations_path(current_user.default_team)
+        elsif current_user.teams.count == 1
+          redirect_to team_nominations_path(current_user.teams.first)
         else
           redirect_to team_selector_path
         end
